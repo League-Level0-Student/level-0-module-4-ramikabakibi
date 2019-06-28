1ffbc18411f1b367c2f6078a57784b6deac80648
League Student <league@iMac.attlocal.net>
2019-06-27 20:00:10 -0700

    Finished: Sleepy Head, String Methods
    :sunglasses:

--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -17,12 +17,27 @@ public class SleepyHead {
         /*
          * Ask the user for these values using a confirm dialog like the one below*/
         int answer= JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Vacation",  JOptionPane.YES_NO_OPTION);
+        
+         System.out.println(answer);
+         isWeekday=answer==0;
          
-    
 
+int vacation=JOptionPane.showConfirmDialog(null, "Is it a vacation?");
+System.out.println(vacation);
+isVacation=vacation==0;
+System.out.println(isWeekday);
+System.out.println(isVacation);
+if((isWeekday)&&(!isVacation)) {
+	JOptionPane.showMessageDialog(null, "Get up lazybones!");
+	
+}
+else if((!isWeekday)||(isVacation)); {
+	JOptionPane.showMessageDialog(null, "You can sleep in");
+	
+}
 
         /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
+         * (Got this one)Print “sleep in�? if it is a vacation or a weekend. (Got this one)If it’s a weekday,
          * print “get up lazybones!�? If it is a weekday, and we are on vacation,
          * print “sleep in�?.
          */
